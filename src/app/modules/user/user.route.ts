76ea56792fcc229229dd60ddca58a51ee8eb8322
AbdulSatterism <abdulsatter.ism@gmail.com>
2025-09-20 17:44:51 +0600

    update route in post to patch in user update route

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -17,7 +17,7 @@ router.post(
 
 router.get('/all-user', auth(USER_ROLES.ADMIN), UserController.getAllUser);
 
-router.post(
+router.patch(
   '/update-profile',
   fileUploadHandler(),
   auth(USER_ROLES.USER, USER_ROLES.ADMIN),
