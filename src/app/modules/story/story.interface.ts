065ec0795f45578347c03a5ced58e4c8a9ddeda4
AbdulSatterism <abdulsatter.ism@gmail.com>
2026-02-14 11:43:20 +0600

    caption added in the story

--- src/app/modules/story/story.interface.ts
+++ src/app/modules/story/story.interface.ts
@@ -1,21 +1,22 @@
 import { Types } from 'mongoose';
 
-export interface IStory {
-  video?: string;
-  image?: string;
-  contentType: 'image' | 'video';
-  author: Types.ObjectId;
-  expiryNotificationSent?: boolean;
-}
+// export interface IStory {
+//   video?: string;
+//   image?: string;
+//   contentType: 'image' | 'video';
+//   author: Types.ObjectId;
+//   expiryNotificationSent?: boolean;
+// }
 
 export interface IStory {
   video?: string;
   image?: string;
   contentType: 'image' | 'video';
+  caption?: string;
   author: Types.ObjectId;
   expiryNotificationSent?: boolean;
-  video_ios?: string; // iPhone (original master / HDR master)
-  video_normal?: string; // Android/normal (SDR playback)
-  image_ios?: string; // iPhone (original)
-  image_normal?: string; // Android/normal (optimized)
+  video_ios?: string;
+  video_normal?: string;
+  image_ios?: string;
+  image_normal?: string;
 }
--- src/app/modules/story/story.model.ts
+++ src/app/modules/story/story.model.ts
@@ -6,6 +6,7 @@ const storySchema = new Schema<IStory>(
     image: { type: String, default: '' },
     video: { type: String, default: '' },
     contentType: { type: String, enum: ['image', 'video'], required: true },
+    caption: { type: String, default: '' },
     author: { type: Schema.Types.ObjectId, ref: 'User', required: true },
     expiryNotificationSent: { type: Boolean, default: false },
     video_ios: { type: String, default: '' },
